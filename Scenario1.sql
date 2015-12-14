80c38029fe01fa5dcc67a6d5dbaa02ed9d003a8b
John Ryan <john.ryan@cardinalhealth.com>
2015-12-14 13:53:12 -0500

    Scenario1 complete

--- Scenario1.sql
+++ Scenario1.sql
@@ -5,3 +5,40 @@
 */
 USE Kata;
 GO
+
+
+-- To check on the situation.
+SELECT
+    rowguid,
+    COUNT(*)
+FROM
+    sales.SalesOrderDetail
+GROUP BY
+    rowguid
+HAVING
+    COUNT(*) > 1
+
+SELECT
+    *
+FROM
+    sales.SalesOrderDetail
+WHERE
+    rowguid = '76F3EE0C-D79A-400F-88C5-67B84C3B5880';
+
+-- Now that we have identified the issue, the below should remove 1000 records.
+WITH    dupes
+          AS ( SELECT
+                rowguid,
+                ROW_NUMBER() OVER ( PARTITION BY rowguid ORDER BY Sales.SalesOrderDetail.SalesOrderDetailID ) dupe_order
+               FROM
+                sales.SalesOrderDetail)
+    DELETE
+        dupes
+    WHERE
+        dupe_order > 1;
+GO
+
+-- To Prevent this moving forward
+ALTER TABLE sales.SalesOrderDetail
+ADD CONSTRAINT AK_SalesOrderDetail_Rowguid UNIQUE (ROWGUID);
+
